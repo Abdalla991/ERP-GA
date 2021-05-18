b5f810fd0ec0f3651cf040e219ec3d29050aae90
MohamedAyman3600 <moh.ayman3600@gmail.com>
2021-05-18 13:09:28 +0200

    Fixed if statment after for loop to display all unavailable products

--- customaddons/yds_mrp2/models/yds_mrp_production.py
+++ customaddons/yds_mrp2/models/yds_mrp_production.py
@@ -1,23 +1,28 @@
-from odoo import models, fields, api, exceptions,_
+from odoo import models, fields, api, exceptions, _
 from odoo.exceptions import ValidationError
 from collections import defaultdict
-import ipdb 
+import ipdb
 from datetime import datetime
+
 
 class YdsMrpProduction(models.Model):
     _inherit = "mrp.production"
-    yds_bom_expired = fields.Boolean(string="Selected Bill of Material has expired !", readonly=True, Default="False")
+    yds_bom_expired = fields.Boolean(
+        string="Selected Bill of Material has expired !", readonly=True, Default="False")
 
     @api.depends('product_id', 'bom_id', 'company_id')
     def _compute_allowed_product_ids(self):
-    
-        for production in self: 
+
+        for production in self:
             if production.bom_id:
                 if production.bom_id.yds_start_date and production.bom_id.yds_end_date:
-                    start = datetime.strptime(str(production.bom_id.yds_start_date), '%Y-%m-%d')
-                    end = datetime.strptime(str(production.bom_id.yds_end_date), '%Y-%m-%d')
-                    current = datetime.strptime(str(datetime.now().date()), '%Y-%m-%d')
-                    if start <= current and end > current :
+                    start = datetime.strptime(
+                        str(production.bom_id.yds_start_date), '%Y-%m-%d')
+                    end = datetime.strptime(
+                        str(production.bom_id.yds_end_date), '%Y-%m-%d')
+                    current = datetime.strptime(
+                        str(datetime.now().date()), '%Y-%m-%d')
+                    if start <= current and end > current:
                         production.yds_bom_expired = False
                         print(str(production.yds_bom_expired))
                     else:
@@ -25,23 +30,24 @@ class YdsMrpProduction(models.Model):
                         print(str(production.yds_bom_expired))
 
         return super(YdsMrpProduction, self)._compute_allowed_product_ids()
-    
+
     # def button_mark_done(self):
     #     for move in self.move_raw_ids:
     #         if move.forecast_availability < move.quantity_done:
     #             raise ValidationError(_('Please check the availability for all components.'))
     #             return
     #     res = super(YdsMrpProduction, self).button_mark_done()
-    #     return res		
-    
+    #     return res
+
     # def button_mark_done(self):
     #     unavailable_components = []
     #     for move in self.move_raw_ids:
-    #         if move.state != 'assigned' or move.state != "done":
+    #         if move.state not in ['assigned', 'done']:
     #             unavailable_components.append(move.product_id.name)
-    #         if len(unavailable_components) > 0:
-    #             unavail_str = ','.join(unavailable_components)
-    #             raise ValidationError(_(f'Please check the availability for the following components: {unavail_str}.'))
-    #             return
+    #     if len(unavailable_components) > 0:
+    #         unavail_str = ', '.join(unavailable_components)
+    #         raise ValidationError(
+    #             _(f'Please check the availability of the following components: {unavail_str}.'))
+    #         return
     #     res = super(YdsMrpProduction, self).button_mark_done()
-    #     return res		
+    #     return res
